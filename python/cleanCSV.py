c5a76675fa1fbf6fbee6bc75bb6d9b337af21cb0
facaraff <fabio.caraffini@gmail.com>
2022-10-02 11:07:01 +0100

    clean tox files function

--- python/cleanCSV.py
+++ python/cleanCSV.py
@@ -1,14 +1,21 @@
 import csv
-rows = []
-file =  open("../tox/Fathead_minnow_LC50_(96_hr)_Consensus2.csv", 'r')
-csvreader = csv.reader(file,delimiter=",")
-# header = next(csvreader)
-rows.append(next(csvreader))
-for row in csvreader:
-    if  row[4] != "Structure can't be parsed":
-        rows.append(row)
-# print(header)
-print(rows)
-cleanedFile =  open("../tox/cleanedInputs/Cleaned_Fathead_minnow_LC50_(96_hr)_Consensus2.csv", 'w', newline='')
-cleanedCSV = csv.writer(cleanedFile)
-cleanedFile.writerows(rows)
+
+
+def cleanInputFile(fileName):
+    readFrom = "../tox/"
+    rows = []
+    file =  open(readFrom+fileName, 'r')
+    csvreader = csv.reader(file,delimiter=",")
+    # header = next(csvreader)
+    rows.append(next(csvreader))
+    for row in csvreader:
+        if  row[4] != "Structure can't be parsed":
+            rows.append(row)
+    # print(header)
+    #print(rows)
+    cleanedFile =  open(readFrom+"cleanedInputs/Cleaned_"+fileName, 'w', newline='')
+    cleanedCSV = csv.writer(cleanedFile)
+    cleanedCSV.writerows(rows)
+
+
+cleanInputFile("Fathead_minnow_LC50_(96_hr)_Consensus2.csv")
